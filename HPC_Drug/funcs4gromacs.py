76eabc395bcc98c68440249f478ec803c59c797d
MauriceKarrenbrock <karrenbrockmaurice95@gmail.com>
2020-02-12 14:28:59 +0100

    methods to interact with gromacs

--- HPC_Drug/funcs4gromacs.py
+++ HPC_Drug/funcs4gromacs.py
@@ -183,16 +183,30 @@ class GromacsInput(object):
         return filename
     
 
+    def interact_with_gromacs(self, string = None):
+        """Interacts with gromacs running string
+        with subprocess.run
+        string must contain the gromacs path"""
+
+        print("Running Gromacs")
+        r = subprocess.run(string,
+                        shell = True,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                        universal_newlines=True)
+
+        print(r.stdout)
+        print(r.stderr)
+
+        if r.returncode != 0:
+            raise Exception(f"Gromacs failure\n{r.stdout}\n{r.stderr}")
+
+
     def execute(self,
                 template = None,
                 filename = None,
                 output_file = None,
-                MD_program_path = None,
                 command_string = None):
-
-        #must be adapted better
-        if MD_program_path == None:
-            MD_program_path = self.MD_program_path
 
         if template == None:
             template = self.template
@@ -208,18 +222,7 @@ class GromacsInput(object):
 
         filename = self.write_template_on_file(template, filename)
 
-        print("Running Gromacs")
-        r = subprocess.run([f'{MD_program_path} {command_string}'],
-                        shell = True,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        universal_newlines=True)
-
-        print(r.stdout)
-        print(r.stderr)
-
-        if r.returncode != 0:
-            raise Exception(f"Gromacs failure\n{r.stdout}\n{r.stderr}")
+        self.interact_with_gromacs(string = command_string)
  
         return output_file
 
@@ -242,22 +245,20 @@ class GromacsMakeProteinGroTop(GromacsInput):
                 Protein = Protein,
                 Ligand = Ligand,
                 protein_tpg_file = protein_tpg_file,
-                solvent_model = solvent_model,
-                MD_program_path = MD_program_path)
+                solvent_model = solvent_model)
         #protein_tpg_file is actually the protein model chosen through choices_file
         
-        self.command_string = f"{MD_program_path} pdb2gmx -f {Protein.filename} -o {Protein.protein_id}.gro -p {Protein.protein_id}.top < {output_filename}"
+        self.command_string = f"{self.MD_program_path} pdb2gmx -f {self.Protein.filename} -o {self.Protein.protein_id}.gro -p {self.Protein.protein_id}.top < {self.output_filename}"
 
     
         self.template = [
                         f"{self.protein_tpg_file}",
                         f"{self.solvent_model}"]
-
+    
     def execute(self,
             template = None,
             filename = None,
             output_file = None,
-            MD_program_path = None,
             command_string = None,
             Protein = None):
 
@@ -266,7 +267,6 @@ class GromacsMakeProteinGroTop(GromacsInput):
         choices_file = super().execute(template = template,
                         filename = filename,
                         output_file = output_file,
-                        MD_program_path = MD_program_path,
                         command_string = command_string)
         choices_file = None
 
@@ -318,17 +318,7 @@ class GromacsMakeJoinedProteinLigandTopGro(GromacsInput):
                                                         output_filename = self.output_filename)
         
         #make a new joined gro file
-        r = subprocess.run([f'{MD_program_path} editconf -f {Protein.filename} -o {Protein.protein_id}_joined.gro'],
-                        shell = True,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        universal_newlines=True)
-
-        print(r.stdout)
-        print(r.stderr)
-
-        if r.returncode != 0:
-            raise Exception(f"Gromacs failure\n{r.stdout}\n{r.stderr}")
+        self.interact_with_gromacs(string = f'{MD_program_path} editconf -f {Protein.filename} -o {Protein.protein_id}_joined.gro')
 
         Protein.gro_file = f"{Protein.protein_id}_joined.gro"
 
@@ -350,6 +340,8 @@ class GromacsMakeJoinedProteinLigandTopGro(GromacsInput):
         with open(f'{Protein.protein_id}_joined.top', 'w') as f:
             top.insert(0, itp_insertion_string)
             top.insert(-1, compound_string)
+            for line in top:
+                f.write(f"{line}\n")
 
         Protein.top_file = f'{Protein.protein_id}_joined.top'
         
@@ -424,8 +416,9 @@ class GromacsSolvBoxInput(GromacsInput):
                 Ligand = None,
                 protein_tpg_file = None,
                 ligand_itp = None,
-                solvent_model = None,
-                MD_program_path = 'gmx'):
+                solvent_model = "amber99sb-ildn.ff/spce.itp",
+                MD_program_path = 'gmx',
+                box_borders = '1'):
 
         super().__init__(input_filename = input_filename,
                         output_filename = output_filename,
@@ -437,13 +430,210 @@ class GromacsSolvBoxInput(GromacsInput):
                         MD_program_path = MD_program_path)
 
         if self.output_filename == None:
-            self.output_filename = f'{self.Protein.protein_id}_solv_box.gro'
+            self.output_filename = f'{self.Protein.protein_id}_solv_box.mdp'
 
         if self.input_filename == None:
-            self.input_filename = f"{self.Protein.protein_id}_solv_box.mdp"
+            self.input_filename = f"{self.Protein.protein_id}_solv_box.gro"
+        
+        self.box_borders = box_borders
 
-        #creates the .tpr and then optimizes the structure
-        self.command_string = f"{self.MD_program_path} grompp -f {self.input_filename}.mdp -c {Protein.gro_file} -p {Protein.top_file} -o {Protein.protein_id}_joined_optimized.tpr -maxwarn 100 && gmx mdrun -s {Protein.protein_id}_joined_optimized.tpr  -c {self.output_filename}"
+        #creates the .tpr and then optimizes the structure it is actually a list of strings
+        self.command_string = [
+            f"{self.MD_program_path} editconf -f {self.Protein.gro_file} -d {self.box_borders} -bt triclinic -angles 90 90 90 -o {self.input_filename}",
+            f"{self.MD_program_path} solvate -cp {self.input_filename} -p {self.Protein.top_file}",
+            f"{self.MD_program_path} grompp -f {self.output_filename} -c {self.Protein.gro_file} -p {self.Protein.top_file} -maxwarn 100 -o {self.Protein.protein_id}_solv_box.tpr",
+            f"{self.MD_program_path} mdrun -s {self.Protein.protein_id}_solv_box.tpr  -c {self.input_filename}"
+                       ]
 
-        self.template = []
-   
+        self.template = [
+                        "; VARIOUS PREPROCESSING OPTIONS",
+                        "; Preprocessor information: use cpp syntax.",
+                        "; e.g.: -I/home/joe/doe -I/home/mary/roe",
+                        "include                  =",
+                        "; e.g.: -DPOSRES -DFLEXIBLE (note these variable names are case sensitive)",
+                        "define                   =",
+                        "",
+                        "; RUN CONTROL PARAMETERS",
+                        "integrator               = md",
+                        "; Start time and timestep in ps",
+                        "tinit                    = 0",
+                        "dt                       = 0.0001",
+                        "nsteps                   = 5000000",
+                        "; For exact run continuation or redoing part of a run",
+                        "init-step                = 0",
+                        "; Part index is updated automatically on checkpointing (keeps files separate)",
+                        "simulation-part          = 1",
+                        "; mode for center of mass motion removal",
+                        "comm-mode                = Linear",
+                        "; number of steps for center of mass motion removal",
+                        "nstcomm                  = 100",
+                        "; group(s) for center of mass motion removal",
+                        "comm-grps                =",
+                        "",
+                        "; TEST PARTICLE INSERTION OPTIONS",
+                        "rtpi                     = 0.05",
+                        "",
+                        "; OUTPUT CONTROL OPTIONS",
+                        "; Output frequency for coords (x), velocities (v) and forces (f)",
+                        "nstxout                  = 1000",
+                        "nstvout                  = 1000",
+                        "nstfout                  = 1000",
+                        "; Output frequency for energies to log file and energy file",
+                        "nstlog                   = 1000",
+                        "nstcalcenergy            = 100",
+                        "nstenergy                = 1000",
+                        "; Output frequency and precision for .xtc file",
+                        "nstxtcout                = 100",
+                        "xtc-precision            = 1000",
+                        "; This selects the subset of atoms for the .xtc file. You can",
+                        "; select multiple groups. By default all atoms will be written.",
+                        "xtc-grps                 =",
+                        "; Selection of energy groups",
+                        "energygrps               = System",
+                        "",
+                        "; NEIGHBORSEARCHING PARAMETERS",
+                        "; cut-off scheme (group: using charge groups, Verlet: particle based cut-offs)",
+                        "; nblist update frequency",
+                        "cutoff-scheme            = Verlet",
+                        "nstlist                  = 20",
+                        "verlet-buffer-tolerance  = 0.0001",
+                        "; ns algorithm (simple or grid)",
+                        "ns_type                  = grid",
+                        "; Periodic boundary conditions: xyz, no, xy",
+                        "pbc                      = xyz",
+                        "periodic-molecules       = no",
+                        "; Allowed energy drift due to the Verlet buffer in kJ/mol/ps per atom,",
+                        "; a value of -1 means: use rlist",
+                        "; nblist cut-off",
+                        "rlist                    = 1.0",
+                        "; long-range cut-off for switched potentials",
+                        "rlistlong                = -1",
+                        "",
+                        "; OPTIONS FOR ELECTROSTATICS AND VDW",
+                        "; Method for doing electrostatics",
+                        "coulombtype              = PME",
+                        "rcoulomb-switch          = 0",
+                        "rcoulomb                 = 1.0",
+                        "; Relative dielectric constant for the medium and the reaction field",
+                        "epsilon-r                = 1",
+                        "epsilon-rf               = 0",
+                        "; Method for doing Van der Waals",
+                        "vdw-type                 = Cut-off",
+                        "; cut-off lengths",
+                        "rvdw-switch              = 0",
+                        "rvdw                     = 1.0",
+                        "; Apply long range dispersion corrections for Energy and Pressure",
+                        "DispCorr                 = EnerPres",
+                        "; Extension of the potential lookup tables beyond the cut-off",
+                        "table-extension          = 1",
+                        "; Separate tables between energy group pairs",
+                        "energygrp-table          =",
+                        "; Spacing for the PME/PPPM FFT grid",
+                        "fourierspacing           = 0.12",
+                        "; FFT grid size, when a value is 0 fourierspacing will be used",
+                        "fourier-nx               = 0",
+                        "fourier-ny               = 0",
+                        "fourier-nz               = 0",
+                        "; EWALD/PME/PPPM parameters",
+                        "pme-order                = 4",
+                        "ewald-rtol               = 1e-06",
+                        "ewald-geometry           = 3d",
+                        "epsilon-surface          =",
+                        "optimize-fft             = no",
+                        "",
+                        "; IMPLICIT SOLVENT ALGORITHM",
+                        "implicit-solvent         = No",
+                        "",
+                        "; OPTIONS FOR WEAK COUPLING ALGORITHMS",
+                        "; Temperature coupling",
+                        "tcoupl                   = v-rescale",
+                        "nsttcouple               = -1",
+                        "nh-chain-length          = 1",
+                        "; Groups to couple separately",
+                        "tc-grps                  = System",
+                        "; Time constant (ps) and reference temperature (K)",
+                        "tau-t                    = 0.2",
+                        "ref-t                    = 298.15",
+                        "; pressure coupling",
+                        "pcoupl                   = Berendsen",
+                        "pcoupltype               = Isotropic",
+                        "nstpcouple               = -1",
+                        "; Time constant (ps), compressibility (1/bar) and reference P (bar)",
+                        "tau-p                    = 0.5",
+                        "compressibility          = 4.6e-5",
+                        "ref-p                    = 1",
+                        "; Scaling of reference coordinates, No, All or COM",
+                        "refcoord-scaling         = COM",
+                        "",
+                        "; GENERATE VELOCITIES FOR STARTUP RUN",
+                        "gen-vel                  = no",
+                        "gen-temp                 = 500",
+                        "gen-seed                 = 173529",
+                        "",
+                        "; OPTIONS FOR BONDS",
+                        "constraints              = none",
+                        "; Type of constraint algorithm",
+                        "constraint-algorithm     = Lincs",
+                        "; Do not constrain the start configuration",
+                        "continuation             = no",
+                        "; Use successive overrelaxation to reduce the number of shake iterations",
+                        "Shake-SOR                = no",
+                        "; Relative tolerance of shake",
+                        "shake-tol                = 0.00001",
+                        "; Highest order in the expansion of the constraint coupling matrix",
+                        "lincs-order              = 5",
+                        "; Number of iterations in the final step of LINCS. 1 is fine for",
+                        "; normal simulations, but use 2 to conserve energy in NVE runs.",
+                        "; For energy minimization with constraints it should be 4 to 8.",
+                        "lincs-iter               = 2",
+                        "; Lincs will write a warning to the stderr if in one step a bond",
+                        "; rotates over more degrees than",
+                        "lincs-warnangle          = 30",
+                        "; Convert harmonic bonds to morse potentials",
+                        "morse                    = no"
+                        ]
+
+    def _edit_top_file(self, Protein, water_itp = None):
+        """Adds the needed #include for water
+        to the protein top file"""
+
+        if water_itp == None:
+            water_itp = "amber99sb-ildn.ff/spce.itp"
+
+        with open(Protein.top_file, 'r') as f:
+            top = f.readlines()
+
+        itp_insertion_string = f'#include "{water_itp}"'
+
+        with open(Protein.top_file, 'w') as f:
+            top.insert(0, itp_insertion_string)
+            for line in top:
+                f.write(f"{line}\n")
+
+        
+        return Protein
+
+    def execute(self,
+                template = None,
+                filename = None,
+                output_file = None,
+                command_string = None):
+
+        if template == None:
+            template = self.template
+
+        if filename == None:
+            filename = self.output_filename
+        
+        if output_file == None:
+            output_file = self.Protein.filename.rsplit('.', 1)[0].strip() + '_unnamed_output'
+        
+        if command_string == None:
+            command_string = self.command_string
+
+        filename = self.write_template_on_file(template, filename)
+
+        for string in command_string:
+            self.interact_with_gromacs(string = string)
+ 
+        return output_file
