06a4a3e53344b8d7260a05f537e8cb86387df2ba
MauriceKarrenbrock <56843667+MauriceKarrenbrock@users.noreply.github.com>
2020-03-25 21:57:28 +0100

    test append_file()

--- tests/test_write_on_files.py
+++ tests/test_write_on_files.py
@@ -72,3 +72,93 @@ class test_write_file(unittest.TestCase):
         finally:
 
             os.remove(output_file)
+
+
+def _create_input_file():
+    """private"""
+
+    file_name = 'tests/files4tests/file_to_append.txt'
+    string = "aaa"
+
+    write_on_files.write_file(lines = string, file = file_name)
+
+    return file_name, string
+
+class test_append_file(unittest.TestCase):
+
+    def test_wrong_input(self):
+
+        wrong_inputs = (1, 1.5)
+
+        with self.assertRaises(TypeError):
+
+            for i in wrong_inputs:
+                
+                write_on_files.append_file(lines = i, file = "dummy")
+
+    
+    def test_with_string(self):
+
+        input_file, existing_string = _create_input_file()
+
+        small_string = "string"
+        string = f"{small_string}\n{small_string}"
+
+        try:
+            write_on_files.append_file(lines = string, file = input_file)
+
+            with open(input_file, 'r') as f:
+                lines = f.readlines()
+
+            self.assertEqual(lines, [existing_string + small_string + '\n', small_string])
+        finally:
+            
+            os.remove(input_file)
+
+    def test_with_list(self):
+
+        input_lists = (["string", "string"], ["\nstring\n", "string\n"])
+        
+
+        for i in range(len(input_lists)):
+
+            input_file, existing_string = _create_input_file()
+            expected_ouputs = ([existing_string + "stringstring"], [existing_string + '\n', "string\n", "string\n"])
+
+            try:
+
+                write_on_files.append_file(lines = input_lists[i], file = input_file)
+
+                with open(input_file, 'r') as f:
+                    lines = f.readlines()
+
+                self.assertEqual(lines, expected_ouputs[i])
+
+            finally:
+
+                os.remove(input_file)
+
+    def test_with_tuple(self):
+
+        input_tuples = (("string", "string"), ("\nstring\n", "string\n"))
+        
+
+        for i in range(len(input_tuples)):
+
+            input_file, existing_string = _create_input_file()
+            expected_ouputs = ([existing_string + "stringstring"], [existing_string + '\n', "string\n", "string\n"])
+
+            try:
+
+            
+                write_on_files.append_file(lines = input_tuples[i], file = input_file)
+
+                with open(input_file, 'r') as f:
+                    lines = f.readlines()
+
+                self.assertEqual(lines, expected_ouputs[i])
+
+            finally:
+
+                os.remove(input_file)
+
