db9fa2b066751342accaad0e4ef139139eec4449
MauriceKarrenbrock <56843667+MauriceKarrenbrock@users.noreply.github.com>
2020-04-05 12:37:18 +0200

    created & tested get_ligands.py

---
+++ HPC_Drug/structures/get_ligands.py
@@ -0,0 +1,67 @@
+"""
+Copyright (c) 2020-2020 Maurice Karrenbrock
+
+This software is open-source and is distributed under the
+GNU Affero General Public License v3 (agpl v3) license
+
+A copy of the license must be included with any copy of the program or part of it
+"""
+
+# The function in this file updates Protein._ligands
+
+from HPC_Drug.structures import ligand
+from HPC_Drug.PDB import prody
+
+def get_ligands(Protein, ligand_resnames_resnums):
+    """
+    Takes a HPC_Drug.structures.protein.Protein instance and a ligand_resnames_resnums
+    and updates Protein._ligands with the newly created HPC_Drug.structures.ligand.Ligand
+    instances
+
+    Protein :: HPC_Drug.structures.protein.Protein, Protein.file_type must be pdb !!!
+
+    ligand_resnames_resnums :: nested list of type [ ['ligand_resname', ligand_resnum], [...], ... ]
+    if == None or == [] no HPC_Drug.structures.ligand.Ligand instance will be added to Protein._ligands
+
+    return Protein
+    """
+
+    #There are no ligands to add
+    if ligand_resnames_resnums == None or ligand_resnames_resnums == []:
+        
+        return Protein
+
+    if Protein.file_type != 'pdb':
+        raise TypeError(f"This function works only on PDB files, not {Protein.file_type}")
+
+    #check if ligand_resnames_resnums is a nested list as it should be
+    try:
+        ligand_resnames_resnums[0][0]
+    except:
+        ligand_resnames_resnums = [ligand_resnames_resnums]
+
+
+    Protein.update_structure(struct_type = "prody")
+
+    prody_select = prody.ProdySelect(structure = Protein.structure)
+
+    for i, ligand_name_number in enumerate(ligand_resnames_resnums):
+
+        Ligand = ligand.Ligand(
+            resname = ligand_name_number[0],
+            pdb_file = None,
+            structure = None,
+            file_type = 'pdb',
+            tpg_file = None,
+            prm_file = None,
+            resnum = ligand_name_number[1]
+        )
+
+        Ligand.structure = prody_select.resnum(resnum = Ligand.resnum)
+
+        Ligand.write(file_name = f"{Ligand.resname}_lgand{i}.pdb", struct_type = "prody")
+
+        Protein.add_ligand(Ligand = Ligand)
+
+    return Protein
+
---
+++ tests/structures/test_get_ligands.py
@@ -0,0 +1,73 @@
+"""
+Copyright (c) 2020-2020 Maurice Karrenbrock
+
+This software is open-source and is distributed under the
+GNU Affero General Public License v3 (agpl v3) license
+
+A copy of the license must be included with any copy of the program or part of it
+"""
+
+import unittest
+import unittest.mock as mock
+
+from HPC_Drug.structures import get_ligands
+
+class test_get_ligands(unittest.TestCase):
+
+    def test_reise_typeError(self):
+
+        Protein = mock.Mock()
+
+        Protein.file_type = "WRONG_TYPE"
+
+        with self.assertRaises(TypeError):
+
+            get_ligands.get_ligands(Protein = Protein, ligand_resnames_resnums = [["LIG", 1]])
+
+    def test_empty_liglist(self):
+
+        Protein = mock.Mock()
+
+        Protein.file_type = "pdb"
+
+        output = get_ligands.get_ligands(Protein = Protein, ligand_resnames_resnums = [])
+
+        output.add_ligand.assert_not_called()
+
+        self.assertEqual(Protein, output)
+
+    def test_None_liglist(self):
+
+        Protein = mock.Mock()
+
+        Protein.file_type = "pdb"
+
+        output = get_ligands.get_ligands(Protein = Protein, ligand_resnames_resnums = None)
+
+        output.add_ligand.assert_not_called()
+
+        self.assertEqual(Protein, output)
+
+    def test_with_valid_liglist(self):
+        #should be done better
+
+        with mock.patch("HPC_Drug.structures.ligand.Ligand", autospec = True) as mocked_lig:
+
+            with mock.patch("HPC_Drug.PDB.prody.ProdySelect", autospec = True) as mocked_select:
+
+                mocked_lig.return_value = mocked_lig
+
+                mocked_lig.resnum = 0
+                mocked_lig.resname = "LIG"
+
+                Protein = mock.Mock()
+
+                Protein.file_type = "pdb"
+
+                get_ligands.get_ligands(Protein = Protein, ligand_resnames_resnums = [["LIG", 1], ["LIG", 2]])
+
+                mocked_lig.assert_called()
+
+                mocked_select.assert_called()
+
+                Protein.add_ligand.assert_called()
