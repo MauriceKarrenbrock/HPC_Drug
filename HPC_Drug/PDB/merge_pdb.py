413a14b5db6fc65734e92ec3c7294926dc8af069
MauriceKarrenbrock <56843667+MauriceKarrenbrock@users.noreply.github.com>
2020-04-06 19:33:23 +0200

    created & tested merge_pdb.py

---
+++ HPC_Drug/PDB/merge_pdb.py
@@ -0,0 +1,82 @@
+######################################################################################
+# Copyright (c) 2020-2020 Maurice Karrenbrock                                        #
+#                                                                                    #
+# This software is open-source and is distributed under the                          #
+# GNU Affero General Public License v3 (agpl v3 license)                             #
+#                                                                                    #
+# A copy of the license must be included with any copy of the program or part of it  #
+######################################################################################
+
+"""
+This file contains the functions to merge PDB or mmCIF
+They are useful wen you need to merge one or more organic ligands with a protein
+"""
+
+from HPC_Drug.files_IO import read_file
+from HPC_Drug.files_IO import write_on_files
+from HPC_Drug.auxiliary_functions import path
+
+
+def merge_pdb(Protein):
+    """
+    Will put all the given ligands after the protein and update the ligand resnums
+    this function is brutal and memory consuming I should do it better in the future
+
+    both th protein and the ligands should be in PDB files (no check will be done)
+
+    Protein :: HPC_Drug.structures.protein.Protein instance with a valid _ligands value
+
+    return Protein with updated Protein.pdb_file
+    """
+
+    protein_file = read_file.read_file(file_name = Protein.pdb_file)
+
+    ligands = Protein.get_ligand_list()
+
+    #get the index of the line with the last ATOM HETATM or TER line
+    #and get the resnum of this last residue
+    for i in range(len(protein_file) - 1, -1 , -1):
+
+        if protein_file[i][0:4] == 'ATOM' or protein_file[i][0:6] == 'HETATM' or protein_file[i][0:3] == 'TER':
+
+            residue_number =  int(protein_file[i][22:26].strip())
+
+            index_protein_file = i + 1
+
+            break
+
+
+    #create the ligands list of strings
+    ligand_file = []
+    for j in range(len(ligands)):
+
+        residue_number = residue_number + 1
+
+        #update resnum
+        ligands[j].resnum = residue_number
+
+        tmp_ligand = read_file.read_file(file_name = ligands[j].pdb_file)
+
+        #update the residue numbers in the file
+        for k in range(len(tmp_ligand)):
+
+            tmp_ligand[k] = tmp_ligand[k][:22] + "{0:>4}".format(residue_number) + tmp_ligand[k][26:]
+
+        ligand_file = ligand_file + tmp_ligand
+
+
+    #insert the ligands in the right place of the protein_file list
+    protein_file[index_protein_file:index_protein_file] =  ligand_file
+
+    #be sure to get the right formatting
+    for i in range(len(protein_file)):
+
+        protein_file[i] = protein_file[i].strip('\n') + '\n'
+
+    #overwrite Protein.pdb_file
+    write_on_files.write_file(lines = protein_file, file_name = f"{Protein.protein_id}_joined.pdb")
+
+    Protein.pdb_file = path.absolute_filepath(path = f"{Protein.protein_id}_joined.pdb")
+
+    return Protein
+
---
+++ tests/PDB/test_merge_pdb.py
@@ -0,0 +1,88 @@
+######################################################################################
+# Copyright (c) 2020-2020 Maurice Karrenbrock                                        #
+#                                                                                    #
+# This software is open-source and is distributed under the                          #
+# GNU Affero General Public License v3 (agpl v3 license)                             #
+#                                                                                    #
+# A copy of the license must be included with any copy of the program or part of it  #
+######################################################################################
+
+import unittest
+import unittest.mock as mock
+
+from HPC_Drug.PDB import merge_pdb
+
+class test_merge_pdb(unittest.TestCase):
+
+    def test_with_correct_input(self):
+
+        input_list = [
+            ["ATOM   1286  CA  LYS B   1      20.249  -7.881   8.457  1.00 65.74           C\n",
+            "ATOM   1286  CA  LYS B   2      20.249  -7.881   8.457  1.00 65.74           C\n",
+            "CONNETC1286  CA  LYS B  -1      20.249  -7.881   8.457  1.00 65.74           C\n"],
+
+            ["ATOM   1286  CA  LYS B   1      20.249  -7.881   8.457  1.00 65.74           C\n",
+            "HETATM 1286  CA  LYS B   2      20.249  -7.881   8.457  1.00 65.74           C\n",
+            "CONNETC1286  CA  LYS B  -1      20.249  -7.881   8.457  1.00 65.74           C\n"],
+
+            ["ATOM   1286  CA  LYS B   1      20.249  -7.881   8.457  1.00 65.74           C\n",
+            "TER    1286  CA  LYS B   2      20.249  -7.881   8.457  1.00 65.74           C\n",
+            "CONNETC1286  CA  LYS B  -1      20.249  -7.881   8.457  1.00 65.74           C\n"]
+        ]
+
+        expected_output = [
+            ["ATOM   1286  CA  LYS B   1      20.249  -7.881   8.457  1.00 65.74           C\n",
+            "ATOM   1286  CA  LYS B   2      20.249  -7.881   8.457  1.00 65.74           C\n",
+            "HETATM   86  CA  LIG B   3      20.000  -7.881   8.457  1.00 65.74           C\n",
+            "HETATM   86  CA  LIG B   4      20.000  -7.881   8.457  1.00 65.74           C\n",
+            "CONNETC1286  CA  LYS B  -1      20.249  -7.881   8.457  1.00 65.74           C\n"],
+
+            ["ATOM   1286  CA  LYS B   1      20.249  -7.881   8.457  1.00 65.74           C\n",
+            "HETATM 1286  CA  LYS B   2      20.249  -7.881   8.457  1.00 65.74           C\n",
+            "HETATM   86  CA  LIG B   3      20.000  -7.881   8.457  1.00 65.74           C\n",
+            "HETATM   86  CA  LIG B   4      20.000  -7.881   8.457  1.00 65.74           C\n",
+            "CONNETC1286  CA  LYS B  -1      20.249  -7.881   8.457  1.00 65.74           C\n"],
+
+            ["ATOM   1286  CA  LYS B   1      20.249  -7.881   8.457  1.00 65.74           C\n",
+            "TER    1286  CA  LYS B   2      20.249  -7.881   8.457  1.00 65.74           C\n",
+            "HETATM   86  CA  LIG B   3      20.000  -7.881   8.457  1.00 65.74           C\n",
+            "HETATM   86  CA  LIG B   4      20.000  -7.881   8.457  1.00 65.74           C\n",
+            "CONNETC1286  CA  LYS B  -1      20.249  -7.881   8.457  1.00 65.74           C\n"]
+        ]
+
+        for i in range(len(input_list)):
+
+            with mock.patch("HPC_Drug.files_IO.read_file.read_file", side_effect = [input_list[i],\
+                ["HETATM   86  CA  LIG B0000      20.000  -7.881   8.457  1.00 65.74           C\n"],\
+                    ["HETATM   86  CA  LIG B0000      20.000  -7.881   8.457  1.00 65.74           C\n"]]) as mocked_read:
+
+                with mock.patch("HPC_Drug.files_IO.write_on_files.write_file") as mocked_write:
+
+                    with mock.patch("HPC_Drug.auxiliary_functions.path.absolute_filepath", return_value = "abs_path") as mocked_path:
+
+                        Protein = mock.Mock()
+                        ligand_1 = mock.Mock()
+                        ligand_2 = mock.Mock()
+
+                        ligand_1.pdb_file = "ligand_file"
+                        ligand_2.pdb_file = "ligand_file"
+
+                        ligand_1.resnum = 0
+                        ligand_2.resnum = 0
+
+                        Protein.protein_id = "test_id"
+                        Protein.pdb_file = "protein_file"
+                        Protein.get_ligand_list.return_value = [ligand_1, ligand_2]
+
+
+                        output = merge_pdb.merge_pdb(Protein = Protein)
+
+                        mocked_read.assert_called()
+
+                        mocked_write.assert_called_once_with(lines = expected_output[i], file_name = "test_id_joined.pdb")
+
+                        mocked_path.assert_called_once_with(path = "test_id_joined.pdb")
+
+                        self.assertEqual(output.get_ligand_list()[0].resnum, 3)
+                        self.assertEqual(output.get_ligand_list()[1].resnum, 4)
+                        self.assertEqual(output.pdb_file, "abs_path")
