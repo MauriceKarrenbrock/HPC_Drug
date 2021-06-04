94642c70deb3aaecc2a9db49d75d808a504aa9c2
MauriceKarrenbrock <56843667+MauriceKarrenbrock@users.noreply.github.com>
2021-06-04 12:54:16 +0200

    added -ntmpi 1 to some gromacs mdrun commands, because gromacs did complain on some computers

--- HPC_Drug/MD/gromacs/solv_box.py
+++ HPC_Drug/MD/gromacs/solv_box.py
@@ -74,7 +74,7 @@ class GromacsSolvBoxInput(gromacs_input.GromacsInput):
             [f"{self.MD_program_path}", "editconf", "-f", f"{self.Protein.gro_file}", "-d", f"{self.box_borders}", "-bt", f"{box_shape}", "-angles", "90", "90", "90", "-o", f"{self.output_gro_file}"],
             [f"{self.MD_program_path}", "solvate", "-cp", f"{self.output_gro_file}", "-p", f"{self.Protein.top_file}", "-o", f"{self.output_gro_file}"],
             [f"{self.MD_program_path}", "grompp", "-f", f"{self.mdp_file}", "-c", f"{self.output_gro_file}", "-p", f"{self.Protein.top_file}", "-maxwarn", "100", "-o", f"{self.output_tpr_file}"],
-            [f"{self.MD_program_path}", "mdrun", "-s", f"{self.output_tpr_file}", "-c", f"{self.output_gro_file}"]
+            [f"{self.MD_program_path}", "mdrun", "-s", f"{self.output_tpr_file}", "-c", f"{self.output_gro_file}", '-ntmpi', '1']
         ]
 
         self.template = [
@@ -294,7 +294,7 @@ class OptimizeOnlyWaterBox(gromacs_input.GromacsInput):
 
         self.command_string = [
             [f"{self.MD_program_path}", "grompp", "-f", f"{self.mdp_file}", "-c", f"{self.output_gro_file}", "-p", f"{self.output_top_file}", "-maxwarn", "100", "-o", f"{self.output_tpr_file}"],
-            [f"{self.MD_program_path}", "mdrun", "-s", f"{self.output_tpr_file}", "-c", f"{self.output_gro_file}"]
+            [f"{self.MD_program_path}", "mdrun", "-s", f"{self.output_tpr_file}", "-c", f"{self.output_gro_file}", '-ntmpi', '1']
         ]
         
 
