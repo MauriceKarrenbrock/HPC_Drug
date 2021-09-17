ade900c504c8a73b639296a09444240575781b30
MauriceKarrenbrock <56843667+MauriceKarrenbrock@users.noreply.github.com>
2021-09-17 17:26:11 +0200

    renamed HPC_Drug/MD/gromacs/first_opt.py -> HPC_Drug/MD/gromacs/minimization.py and did modifications (no editconf anymore)

--- HPC_Drug/MD/gromacs/minimization.py
+++ HPC_Drug/MD/gromacs/minimization.py
@@ -12,7 +12,7 @@ import os
 from HPC_Drug.MD.gromacs import gromacs_input
 
 
-class GromacsFirstOptimization(gromacs_input.GromacsInput):
+class GromacsMinimization(gromacs_input.GromacsInput):
 
     """Makes the first optimization of the given structure"""
 
@@ -23,20 +23,18 @@ class GromacsFirstOptimization(gromacs_input.GromacsInput):
         super().__init__(Protein = Protein,
                         MD_program_path = MD_program_path)
 
-        self.output_gro_file = os.getcwd() + "/" + f"{self.Protein.protein_id}_firstopt.gro"
+        self.output_gro_file = os.getcwd() + "/" + f"{self.Protein.protein_id}_minimization.gro"
         
-        self.output_pdb_file = os.getcwd() +  "/" + f"{self.Protein.protein_id}_firstopt.pdb"
+        self.output_pdb_file = os.getcwd() +  "/" + f"{self.Protein.protein_id}_minimization.pdb"
 
-        self.mdp_file = os.getcwd() +  "/" + f"{self.Protein.protein_id}_firstopt.mdp"
+        self.mdp_file = os.getcwd() +  "/" + f"{self.Protein.protein_id}_minimization.mdp"
 
-        self.output_tpr_file = os.getcwd() +  "/" + f"{self.Protein.protein_id}_firstopt.tpr"
+        self.output_tpr_file = os.getcwd() +  "/" + f"{self.Protein.protein_id}_minimization.tpr"
 
         #creates the .tpr and then optimizes the structure
         self.command_string = [
 
-            [f"{self.MD_program_path}", "editconf", "-f", f"{self.Protein.gro_file}", "-d", "1", "-bt", "triclinic", "-angles", "90", "90", "90", "-o", f"{self.Protein.gro_file}"],
-
-            [f"{self.MD_program_path}", "grompp", "-f", f"{self.mdp_file}", "-c", f"{self.Protein.gro_file}", "-p", f"{self.Protein.top_file}", "-o", f"{self.output_tpr_file}", "-maxwarn", "100"],
+            [f"{self.MD_program_path}", "grompp", "-f", f"{self.mdp_file}", "-c", f"{self.Protein.pdb_file}", "-p", f"{self.Protein.top_file}", "-o", f"{self.output_tpr_file}", "-maxwarn", "100"],
             
             [f"{self.MD_program_path}", "mdrun", "-s", f"{self.output_tpr_file}", "-c", f"{self.output_gro_file}", '-ntmpi', '1']
         ]
@@ -56,7 +54,7 @@ class GromacsFirstOptimization(gromacs_input.GromacsInput):
 
 
 
-class GromacsFirstOptimizationOnlyLigand(gromacs_input.GromacsInput):
+class GromacsMinimizationOnlyLigand(gromacs_input.GromacsInput):
 
     """Makes the first optimization of all the ligands of a Protein"""
 
@@ -97,8 +95,7 @@ class GromacsFirstOptimizationOnlyLigand(gromacs_input.GromacsInput):
 
             
             command_string = [
-                [f"{self.MD_program_path}", "editconf", "-f", f"{Ligand[i].gro_file}", "-d", "5", "-bt", "triclinic", "-angles", "90", "90", "90", "-o", f"{Ligand[i].gro_file}"],
-                [f"{self.MD_program_path}", "grompp", "-f", f"{self.mdp_file}", "-c", f"{Ligand[i].gro_file}", "-p", f"{Ligand[i].top_file}", "-o", f"{os.getcwd()}/{Ligand[i].resname}_only_ligand.tpr", "-maxwarn", "100"],
+                [f"{self.MD_program_path}", "grompp", "-f", f"{self.mdp_file}", "-c", f"{Ligand[i].pdb_file}", "-p", f"{Ligand[i].top_file}", "-o", f"{os.getcwd()}/{Ligand[i].resname}_only_ligand.tpr", "-maxwarn", "100"],
                 [f"{self.MD_program_path}", "mdrun", "-s", f"{os.getcwd()}/{Ligand[i].resname}_only_ligand.tpr", "-c", f"{os.getcwd()}/{Ligand[i].resname}_only_ligand.gro", '-ntmpi', '1']
             ]
 
