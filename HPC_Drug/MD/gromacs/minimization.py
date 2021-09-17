cb999392e4b23d31c89639b559b752ea61a693d1
MauriceKarrenbrock <56843667+MauriceKarrenbrock@users.noreply.github.com>
2021-09-17 18:10:08 +0200

    added MinimizeOnlyWaterBox class to HPC_Drug/MD/gromacs/minimization.py

--- HPC_Drug/MD/gromacs/minimization.py
+++ HPC_Drug/MD/gromacs/minimization.py
@@ -104,3 +104,59 @@ class GromacsMinimizationOnlyLigand(gromacs_input.GromacsInput):
             Ligand[i].gro_file = os.getcwd() + "/" + f"{Ligand[i].resname}_only_ligand.gro"
 
         return self.Protein
+
+
+class MinimizeOnlyWaterBox(gromacs_input.GromacsInput):
+    """
+    Minimizes a box of solvent
+    as deafault uses a copy of the one found in HPC_Drug.lib
+    """
+
+    def __init__(self,
+                solvent_pdb,
+                solvent_top,
+                MD_program_path = "gmx"):
+
+        self.solvent_pdb = solvent_pdb
+
+        self.solvent_top = solvent_top
+
+        self.MD_program_path = MD_program_path
+
+        self.output_gro_file = os.getcwd() + "/" + f"only_water_{self.solvent_model}_minimization.gro"
+
+        self.mdp_file = os.getcwd() + "/" + f"only_water_{self.solvent_model}_minimization.mdp"
+
+        self.output_tpr_file = os.getcwd() + "/" + f"only_water_{self.solvent_model}_minimization.tpr"
+
+        self.command_string = [
+            [f"{self.MD_program_path}", "grompp", "-f", f"{self.mdp_file}", "-c", f"{self.solvent_pdb}", "-p", f"{self.solvent_top}", "-maxwarn", "100", "-o", f"{self.output_tpr_file}"],
+            [f"{self.MD_program_path}", "mdrun", "-s", f"{self.output_tpr_file}", "-c", f"{self.output_gro_file}", '-ntmpi', '1']
+        ]
+
+
+        self.template = ["integrator	= steep",
+                        "nsteps		= 1000",
+                        "emtol		= 100",
+                        "emstep		= 0.01",
+                        "nstxout 	= 1",
+                        "nstenergy	= 1",
+                        "rlist		= 1",
+                        "coulombtype	= pme",
+                        "vdw-type	= cut-off",
+                        "rvdw		= 1.0",
+                        "rcoulomb	= 1.0",
+                        "constraints	= none"]
+
+    def execute(self):
+        """
+        Returns the updated gro and top files
+
+        return gro_file, top_file
+        """
+
+        self._write_template_on_file()
+
+        self._interact_with_gromacs()
+
+        return self.output_gro_file, self.output_top_file
